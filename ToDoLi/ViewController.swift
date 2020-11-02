24a8b82a0d7192be07ed6eb72f1c5569eec073d9
Anastasiya Ulitina <anastulitina@gmail.com>
2020-11-02 15:25:26 +0300

    Initial Commit

---
+++ ToDoLi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoLi
+//
+//  Created by Анастасия Улитина on 02.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
